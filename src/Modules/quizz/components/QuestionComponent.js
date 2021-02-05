8b32625f952842ae44280a2c75167feaba37e4de
Nicolas <nicobrazzo@gmail.com>
2021-02-05 18:47:00 +0100

    ADD Style to quizz

--- src/Modules/quizz/components/QuestionComponent.js
+++ src/Modules/quizz/components/QuestionComponent.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import '../../../App.scss';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import '../../../Sass/components/_question.scss';
 
 function QuestionComponent(props) {
 
@@ -8,6 +9,8 @@ function QuestionComponent(props) {
     const [currentQuestion, setCurrentQuestion] = useState(0);
     // On Initialise le score
     const [score, setScore] = useState(0);
+    //on initialise une variable de check
+    const [isSelected, setIsSelected] = useState(false);
     // On récupère l'id dans l'url
     let { id } = useParams();
     // On récupère les questions en fonction de l'id
@@ -19,49 +22,63 @@ function QuestionComponent(props) {
         if(!(currentQuestion >= allQuestions.length)) {
             return (
                 <div>
-                    <p>{allQuestions[currentQuestion].question}</p>
+                    <p className="title">{allQuestions[currentQuestion].question}</p>
                 </div>
             )
         }
 
-        return <div>Vous Avez fini</div>
+        return <div><p className="title">Vous Avez fini !</p></div>
     }
 
     const SubmitButton = () => {
-        return(
-            <button onClick={() => {
-                setCurrentQuestion(currentQuestion + 1);
-                const form = document.querySelector("form");
-                const data = new FormData(form);
-                for (const entry of data) {
-                    console.log(entry[1], allQuestions[currentQuestion].correct_response)
-                    if(entry[1] == allQuestions[currentQuestion].correct_response){
-                        setScore(score + 1);
-                    }
-                  };
-                }}>
-                Valider
-            </button>
-        )
+        if(!(currentQuestion >= allQuestions.length)) {
+            return(
+                <button className="button btn_quizz"
+                disabled={!(isSelected)}
+                onClick={() => {
+                    setCurrentQuestion(currentQuestion + 1);
+                    const form = document.querySelector("form");
+                    const data = new FormData(form);
+                    for (const entry of data) {
+                        if(+entry[1] === allQuestions[currentQuestion].correct_response){
+                            setScore(score + 1);
+                        }
+                    };
+                    setIsSelected(false)
+                    }}>
+                    Valider
+                </button>
+            )
+        }    
+        return <button className="button btn_quizz"><Link to="/quizz">Revenir à la page des quizzs</Link></button>
     }
 
 
     // On affiche les réponses ici
     const Responses = () => {
         if(!(currentQuestion >= allQuestions.length)) {
-            const response = allQuestions[currentQuestion].responses.map(value => {
+            const response = allQuestions[currentQuestion].responses.map((value, key) => {
+                // const randomResponse = Math.random() * Math.floor(currentQuestion);
                 return (
-                    <div>
-                        <input type="radio" id={value.response} name="response" value={value.id} />
-                        <label>{value.response}</label>
+                    <div className="input_display" key={key}>
+                        <div>
+                            <label>
+                                <input className="checkbox_custom" type="radio" id={value.response} name="response" value={value.id} onChange={() => setIsSelected(true)}/>
+                            </label>
+                        </div>
+                        <div>
+                            <span>
+                                {value.response}
+                            </span>
+                        </div>
                     </div>
                 )
             });
+
             return(
                 <form>
-                    <div>
+                    <div className="response_container">
                         {response}
-                        <SubmitButton />
                     </div>  
                 </form>
                 
@@ -71,11 +88,22 @@ function QuestionComponent(props) {
         return <></>
     }
 
+
+    const Score = () => {
+        return(
+            <p className="text_score">Score actuel: {score}</p>
+        )
+    }
+
     return (
-        <div>
+        <div className="container">
             <Question />
+            
             <Responses />
-            ton score : {score}
+
+            <SubmitButton />
+            
+            <Score />
         </div>
     )
 }
--- src/Modules/quizz/container/QuizzContainer.js
+++ src/Modules/quizz/container/QuizzContainer.js
@@ -2,26 +2,38 @@ import React from "react";
 import {
     Link,
 } from "react-router-dom";
-
+import '../../../Sass/components/_quizz.scss';
 
 function QuizzContainer(props) {
 
+    const ThemeTitle = () => {
+        return (
+            <p className="title">Choississez un Thème :</p>
+        )
+    }
+
     const Themes = (props) => {
-       const themes = props.data.map((question) => {
+       const themes = props.data.map((question, key) => {
             return (
-                <div>
-                    Thème: <Link to={`/quizz/${question.id}`}>{question.theme}</Link>
+                <div className="theme_item" onClick={() => {window.location.href=`/quizz/${question.id}`}} key={key}>
+                    <div>
+                        <img src={`${process.env.PUBLIC_URL}/assets/img/themes_background/${question.img}`} alt={question.theme} />
+                    </div>
+                    <div>
+                        <Link to={`/quizz/${question.id}`}>{question.theme}</Link>
+                    </div>
                 </div>
             )
         });
 
-        return <div>{themes}</div>
+        return <div className="theme_items">{themes}</div>
     } 
 
     return (
-        <>
+        <div className="container">
+            <ThemeTitle />
             <Themes data={props.questionsData}/>
-        </>
+        </div>
 
     )
 }
